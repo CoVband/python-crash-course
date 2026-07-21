46f56e100ecd61c6983b4d41bf73d09fe6f38da7
Victor Bandinez <vic2001band@gmail.com>
2026-07-20 21:02:54 -0400

    Completed Exercises 2-5 through 2-7

---
+++ famous_quote 2-5.py
@@ -0,0 +1,5 @@
+quote = '"A person who never made a mistake never tried anything new."'
+print(quote)
+print("Albert Einstein once said, " + quote)
+#Now I'll try printing without using a variable
+print('Albert Einstein once said, "A person who never made a mistake never tried anything new."')
---
+++ stripping_names 2-7.py
@@ -0,0 +1,11 @@
+name = "\tKevin    "
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+name= "\n\nKevin     "
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
